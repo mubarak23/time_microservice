01a310762832a17093562ce90d25e05e6fbf08c5
Mubarak Aminu <mubarakaminu340@gmail.com>
2019-01-06 22:47:14 +0100

    complete creating the logic

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require("express");
+var bodyParser = require('body-parser');
 var app = express();
 
 app.use(function(req, res, next){
@@ -11,9 +12,77 @@ app.get("/", function(req, res){
 	res.json("Welcom to the world of NodeJs");
 });
 
-app.get("/api/timestamp/:date_string", function(req, res){
+app.route('/api/timestamp/:date?')
+  .get(function (req, res){
+    
+    var date = null;
+    // parse the date string
+    if (req.params.date !== undefined) {
+
+      // check if it is a unix timestamp...
+      var unixTimestamp = parseInt(req.params.date*1);
+      if (isNaN(unixTimestamp)) {
+        
+        // it's not a unix timestamp string
+        date = new Date(req.params.date);
+      } else {
+        
+        // it is a timestamp
+        date = new Date(unixTimestamp);
+      }
+      
+    } else {
+      
+      // the date string parameter is empty. 
+      // create a new date based on current time 
+      date = new Date(Date.now());
+    }
+    
+    // Initialize the response object, if Date is invalid
+    // this one will be returned
+
+    var response = date == "Invalid Date" ? 
+      { error: "Invalid Date" } :
+      { "unix": date.getTime(),
+        "utc": date.toUTCString()
+      };
+    
+    res.json(response);
+  });
+
+/*app.route("/api/timestamp/:date?")
+	.get(function(req, res){
+			//var date = null;
+			var date = req.params.date;
+			//res.json(date);
+
+		var dataobj;
+		if(date === ''){
+			date = new Date();
+
+			if(isNAN(date) === true){
+				dataobj = {
+					"unix": "date.getTime()",
+					"utc": "date.toUTCString()"
+				};
+				res.json(dataobj);
+			}else{
+
+				dataobj = {
+					"unix": "null",
+					"utc": "invalid date"
+				};
+				res.json(dataobj);
+			}	
+		}
+		//res.json(dataobj);		
+	})*/
+
+/*app.get("/api/timestamp/:date_string", function(req, res){
 		var date = req.params.date_string;
-		vat dataobj;
+		*/
+		//res.json(date);
+		/*var dataobj;
 		if(date === null){
 			date = new Date();
 
@@ -22,17 +91,19 @@ app.get("/api/timestamp/:date_string", function(req, res){
 					"unix": "date.getTime()",
 					"utc": "date.toUTCString()"
 				};
+				res.json(dataobj);
 			}else{
 
 				dataobj = {
 					"unix": "null",
 					"utc": "invalid date"
 				};
+				res.json(dataobj);
 			}	
-		}
-		res.json(dataobj);	
+		}*/
+		//res.json(dataobj);	
 
-})
+//})
 
 
 
