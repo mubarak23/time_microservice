6fb70e5fadd80181daff4268c83568cb278d1bbe
Mubarak Aminu <mubarakaminu340@gmail.com>
2019-01-06 15:59:18 +0100

    create the endpoint for time microservice

--- server.js
+++ server.js
@@ -1,9 +1,21 @@
 const express = require("express");
 var app = express();
 
+app.use(function(req, res, next){
+	console.log("Project is running Sucessfully");
+	next();
+
+})
+
 app.get("/", function(req, res){
 	res.json("Welcom to the world of NodeJs");
 });
+
+app.get("/api/timestamp/:date_string", function(req, res){
+		$data = req.params.date_string;
+			
+
+})
 
 
 
